e5d3fc724ec5a82cbf73e3fa15cf3694086e267d
yankoivanovwork <51404578+yankoivanovwork@users.noreply.github.com>
2019-07-19 17:25:57 +0300

    19July
    Arabic->Roman DONE

---
+++ ArabicToRoman_Converter/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ArabicToRoman_Converter
+{
+    class Program
+    {
+        private static Dictionary<char, int> romanNumbers = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        static void Main(string[] args)
+        {
+            int entryNumber;
+
+            if (int.TryParse(Console.ReadLine(), out entryNumber) & entryNumber >= 1 & entryNumber <= 500)
+            {
+                string convertedRomanNumber = string.Empty;
+
+                convertedRomanNumber += CalculateArabDigits(entryNumber / 100, 2);
+                convertedRomanNumber += CalculateArabDigits(((entryNumber % 100) - (entryNumber % 10)) / 10, 1);
+                convertedRomanNumber += CalculateArabDigits(entryNumber % 10, 0);
+
+                Console.WriteLine(convertedRomanNumber);
+            }
+        }
+
+        private static string CalculateArabDigits(int entryNumber, int digitPosition)
+        {
+            string convertedRomanNumber = string.Empty;
+
+            if (entryNumber != 0)
+            {
+                int dictionaryPosition = digitPosition * 2;
+
+                if (entryNumber <= 3)
+                {
+                    convertedRomanNumber += new string(romanNumbers.Keys.ElementAtOrDefault(dictionaryPosition), entryNumber);
+                }
+                else if (entryNumber >= 4 & entryNumber <= 8)
+                {
+                    if (entryNumber < 5)
+                        convertedRomanNumber += romanNumbers.Keys.ElementAtOrDefault(dictionaryPosition);
+
+                    convertedRomanNumber += romanNumbers.Keys.ElementAtOrDefault(dictionaryPosition + 1);
+
+                    if (entryNumber > 5)
+                        convertedRomanNumber += new string(romanNumbers.Keys.ElementAtOrDefault(dictionaryPosition), entryNumber - 5);
+                }
+                else
+                {
+                    convertedRomanNumber += romanNumbers.Keys.ElementAtOrDefault(dictionaryPosition);
+                    convertedRomanNumber += romanNumbers.Keys.ElementAtOrDefault(dictionaryPosition + 2);
+                }
+            }
+
+            return convertedRomanNumber;
+        }
+    }
+}
---
+++ RomanToArabic_Converter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RomanToArabic_Converter
+{
+    class Program
+    {
+        private static Dictionary<char, int> romanNumbers = new Dictionary<char, int>
+        {
+            { 'I', 1 }, //0
+            { 'V', 5 }, //1
+            { 'X', 10 }, //2
+            { 'L', 50 }, //3
+            { 'C', 100 }, //4
+            { 'D', 500 }, //5
+            { 'M', 1000 } //6
+        };
+
+        static void Main(string[] args)
+        {
+            string entryRomanNumber = Console.ReadLine().ToUpper();
+            int convertedArabicNumber = 0;
+
+            for (int i = 0; i < entryRomanNumber.Length; i++)
+            {
+                //entryRomanNumber.ElementAtOrDefault(i) == romanNumbers.Keys.ElementAtOrDefault(4) ||
+                if (entryRomanNumber.ElementAtOrDefault(i) == romanNumbers.Keys.ElementAtOrDefault(2)) // || entryRomanNumber.ElementAtOrDefault(i) == romanNumbers.Keys.FirstOrDefault())
+                {
+                    if (entryRomanNumber.ElementAtOrDefault(i) == entryRomanNumber.ElementAtOrDefault(i + 1) & entryRomanNumber.ElementAtOrDefault(i + 1) != romanNumbers.Keys.FirstOrDefault())
+                    {
+                        convertedArabicNumber += entryRomanNumber.ElementAtOrDefault(i);
+                        continue;
+                    }
+                    else
+                    {
+                        convertedArabicNumber -= entryRomanNumber.ElementAtOrDefault(i);
+                        continue;
+                    }
+                }
+
+                if (entryRomanNumber.ElementAtOrDefault(i) == romanNumbers.Keys.FirstOrDefault())
+                {
+                    if (entryRomanNumber.ElementAtOrDefault(i + 1) == romanNumbers.Keys.ElementAtOrDefault(1))
+                    {
+                        convertedArabicNumber -= 1;
+                        continue;
+                    }
+                    else
+                    {
+                        convertedArabicNumber += entryRomanNumber.Substring(entryRomanNumber.IndexOf(entryRomanNumber.ElementAtOrDefault(i)),entryRomanNumber.Length- entryRomanNumber.IndexOf(entryRomanNumber.ElementAtOrDefault(i))).Length * 1;
+                        continue;
+                    }
+                }
+
+                convertedArabicNumber += romanNumbers[entryRomanNumber.ElementAtOrDefault(i)];
+            }
+
+            Console.WriteLine(convertedArabicNumber);
+        }
+    }
+}
